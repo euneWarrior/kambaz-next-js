a5638a506c3390169b776366dd14abbf204ce92d
euneWarrior <saadahhryan@gmail.com>
2025-09-10 10:52:45 -0400

    updated to have github link

--- app/Labs/Lab1/page.tsx
+++ app/Labs/Lab1/page.tsx
@@ -304,7 +304,7 @@ imperdiet sit amet eget metus.
 	to get dummy text and here for 
 	<h4>Github</h4>
 	Please &nbsp;
-	<a href="https://www.lipsum.com" id="wd-lipsum">click here</a>
+	<a href="https://github.com/euneWarrior/kambaz-next-js/tree/a1" id="wd-github">click here</a>
 	&nbsp;
 	to get dummy text<br />
 	<br />

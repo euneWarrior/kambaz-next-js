ccc2c0ed8a6f4e64028bb78bc1267cb3e9d1ab96
euneWarrior <saadahhryan@gmail.com>
2025-10-29 11:05:31 -0400

    fixed null

---
+++ app/Labs/Lab4/ReduxExamples/layout.tsx
@@ -0,0 +1,17 @@
+import ReduxProvider from "../store/ReduxProvider";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <ReduxProvider>
+          {children}
+        </ReduxProvider>
+      </body>
+    </html>
+  );
+}
